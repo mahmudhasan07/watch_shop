00dd14039d03d9d4c3557338d882e3b2a8edc2a7
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-02-24 23:39:48 +0600

    Added new json data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { motion } from "framer-motion"
+import { motion, useAnimation } from "framer-motion"
 
 
 function App() {
@@ -30,6 +30,8 @@ function App() {
     }
   }
 
+const control = useAnimation()
+
   return (
     <section>
       <div id='box1' className='flex overflow-hidden border-2 justify-end'>
@@ -42,7 +44,7 @@ function App() {
         <h2>hello</h2>
         </motion.div>
       </div>
-      <div id='box2' className=''>
+      <div id='box2' className=' my-52'>
         <motion.div className='w-52 h-52 rounded-xl p-2  bg-green-700'
         whileHover={{
           scale : 1.1,
@@ -63,7 +65,7 @@ function App() {
         <h2>hello</h2>
         </motion.div>
       </div>
-      <div id='box4' className='my-5 w-52 h-52 overflow-hidden flex justify-between rounded-xl p-2 bg-yellow-400'>
+      <div id='box4' className='my-10 w-52 h-52 overflow-hidden flex justify-between rounded-xl p-2 bg-yellow-400'>
         {
           [1,2,3].map((element,idx)=>{
            return <motion.li key={idx} variants={handlebox4} initial="start" animate="end" transition={{duration:(idx+1)*1, ease: "backOut"}}  className='h-6 w-10 my-2 text-black bg-white'>
@@ -71,6 +73,35 @@ function App() {
             </motion.li>
           })
         }
+      </div>
+      <div id="box5">
+        <div>
+          <button className='btn bg-blue-800 text-white mr-2' onClick={()=> control.start({
+            x: 1200,
+            transition : {duration : 2}
+          })}>Move Right</button>
+          <button onClick={()=>{
+            control.start({
+              x : -500,
+              transition : {duration : 2}
+            })
+          }}  className='btn bg-blue-800 text-white mr-2'>Move Left</button>
+          <button onClick={()=>{
+            control.start({
+              borderRadius : "50%",
+              transition : {duration : 1}
+            })
+          }}   className='btn bg-blue-800 text-white mr-2'>Circle</button>
+          <button  className='btn bg-blue-800 text-white mr-2'>Stop</button>
+        </div>
+        <motion.div animate={control} className='my-10 w-52 h-52 overflow-hidden flex justify-between rounded-xl p-2 bg-yellow-400'>
+
+        </motion.div>
+      </div>
+      <div>
+        <motion.div>
+          
+        </motion.div>
       </div>
     </section>
   )
