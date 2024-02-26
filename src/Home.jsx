7c0fa727d415568d66cde10070155aacba5c7257
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-02-26 19:36:48 +0600

    try to add new animation

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import image1 from "../public/watch_banner.png"
+import image2 from "../public/watch1.png"
+import image3 from "../public/watch2.jpg"
+import image4 from "../public/watch3.png"
+import image5 from "../public/watch4.png"
 
 const Home = () => {
     return (
-        <section>
+        <section className='mt-20'>
+            <div className='space-y-3'>
+                <h1 className='text-8xl font-extrabold'>Welcome to Our Website</h1>
+                <marquee className="text-5xl font-bold">50% off in our regular watches</marquee>
+            </div>
             <div>
                 
             </div>
